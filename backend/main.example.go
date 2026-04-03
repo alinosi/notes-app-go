b26c091f473008ff47f5e8528a1420b157981225
alinosi <mthoriqulfadli@gmail.com>
2026-04-03 21:46:45 +0700

    Refactor main application structure: remove example file, update main.go with routing and dummy data handling

--- backend/main.example.go
+++ backend/main.example.go
@@ -1,28 +1,27 @@
 package main
 
-imprt (
-		"encoding/json"
-		"net/http"
+import (
+	"encoding/json"
+	"net/http"
 )
 
 // users dummy data
-type Users struct {
-	User_id    string `json:"id"`
-	Email      string `json:"email"`
-	Username   string `json:"username"`
-	Password   string `json:"password"`
-	Role 	   string `json:"role"`
-	Created_at string `json:"created_at"`
-	Updated_at string `json:"updated_at"`
-	Deleted_at string `json:"deleted_at"`
+type User struct {
+	User_id string `json:"id"`
+	Email   string `json:"email"`
+	// Username   string `json:"username"`
+	// Password   string `json:"password"`
+	// Role       string `json:"role"`
+	// Created_at string `json:"created_at"`
+	// Updated_at string `json:"updated_at"`
+	// Deleted_at string `json:"deleted_at"`
 }
 
-
 // notes dummy data
-type Notes struct {
+type Note struct {
 	Note_Id    string `json:"id"`
 	User_id    string `json:"email"`
-	Text	   string `json:"text"`
+	Text       string `json:"text"`
 	Created_at string `json:"created_at"`
 	Updated_at string `json:"updated_at"`
 	Deleted_at string `json:"deleted_at"`
@@ -30,11 +29,11 @@ type Notes struct {
 
 func main() {
 
-// 1. ROUTING: Mendaftarkan alamat
+	// 1. ROUTING: Mendaftarkan alamat
 	http.HandleFunc("/tasks", func(w http.ResponseWriter, r *http.Request) {
-		
+
 		// 2. LOGIKA API: Membuat data pura-pura (Dummy)
-		data := Task{ID: 1, Title: "Belajar Arsitektur Go"}
+		data := User{User_id: "abcd-efgh-123", Email: "admin123@gmail.com"}
 
 		// 3. HEADER: Kasih tahu browser kalau ini adalah JSON, bukan HTML
 		w.Header().Set("Content-Type", "application/json")
--- backend/main.exercise.go
+++ backend/main.exercise.go
@@ -5,6 +5,14 @@ import (
 	"net/http"
 )
 
+type Notes struct {
+	notes_id string `json:"id"`
+	notes_id string `json:"id"`
+	notes_id string `json:"id"`
+	notes_id string `json:"id"`
+	notes_id string `json:"id"`
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("ini adalah halaman utama")
