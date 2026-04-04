6b141f4104d034c6b904f005f2509a9d1321f44f
alinosi <mthoriqulfadli@gmail.com>
2026-04-04 21:25:51 +0700

    Refactor notes handler to return a list of notes in JSON format

--- backend/main.exercise.go
+++ backend/main.exercise.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"time"
 )
@@ -10,12 +12,26 @@ type Notes struct {
 	UserID    string     `json:"user_id"`
 	Text      string     `json:"text"`
 	CreatedAt time.Time  `json:"created_at"`
-	UpdatedAt *time.Time `json:"updated_at"`
+	UpdatedAt time.Time  `json:"updated_at"`
 	DeletedAt *time.Time `json:"deleted_at,omitempty"`
 }
 
 func main() {
 	http.HandleFunc("/notes", func(w http.ResponseWriter, r *http.Request) {
-		data := Notes{ID: "abcd-efg", UserID: "user_a", Text: "this is my first note", CreatedAt: time.Now()}
+		var listNotes []Notes
+
+		// Note 1
+		n1 := Notes{ID: "1", UserID: "user_a", Text: "Beli kopi", CreatedAt: time.Now()}
+		// Note 2
+		n2 := Notes{ID: "2", UserID: "user_a", Text: "Belajar Go", CreatedAt: time.Now()}
+
+		listNotes = append(listNotes, n1, n2)
+
+		w.Header().Set("Content-Type", "application/json")
+
+		json.NewEncoder(w).Encode(listNotes)
 	})
+
+	fmt.Println("server sudah berjalan di http://localhost:8080")
+	http.ListenAndServe("localhost:8080", nil)
 }
