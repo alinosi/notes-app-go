44ea93d782e857ceb50c6a7684a64ce4737d703a
alinosi <mthoriqulfadli@gmail.com>
2026-04-05 17:48:41 +0700

    Refactor main.go and main3.go to improve notes handling and response structure

--- backend/main.go
+++ backend/main.go
@@ -1,18 +1,61 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
+
+type Notes struct {
+	ID        string     `json:"id"`
+	UserID    string     `json:"user_id"`
+	Text      string     `json:"text"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at"`
+	DeletedAt *time.Time `json:"deleted_at,omitempty"`
+}
+
+type Message struct {
+	Message string `json:"message"`
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Printf("ini adalah halaman utama")
+		var sliceData []Notes
+
+		n1 := Notes{ID: "abcd-edf", UserID: "user-1", Text: "this is your first note", CreatedAt: time.Now()}
+		n2 := Notes{ID: "abcd-edf", UserID: "user-1", Text: "yesterdey i got new supercar", CreatedAt: time.Now()}
+
+		sliceData = append(sliceData, n1, n2)
+
+		w.Header().Set("Content-type", "application/json")
+
+		json.NewEncoder(w).Encode(sliceData)
+		w.Write([]byte("\nSHA256{this-is-flag}"))
 	})
 	http.HandleFunc("/detail", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Printf("ini adalah halaman utama")
+		var sliceData []Notes
+
+		n1 := Notes{ID: "abcd-edf", UserID: "user-1", Text: "this is your first note", CreatedAt: time.Now()}
+
+		sliceData = append(sliceData, n1)
+
+		w.Header().Set("Content-Type", "application/json")
+
+		// err simulation before using database
+		var err *int
+		err = nil
+
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+		} else {
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(sliceData)
+		}
+		w.Write([]byte("\ndetail page"))
 	})
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Printf("ini adalah halaman utama")
-	})
+
+	fmt.Println("server berhasil dijalankan pada localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
