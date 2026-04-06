11d97cab3a7aac43a6620ca34112ad2ceb3f07f4
alinosi <mthoriqulfadli@gmail.com>
2026-04-06 08:42:35 +0700

    Add login handler with JSON response for authentication

--- backend/main.go
+++ backend/main.go
@@ -55,6 +55,49 @@ func main() {
 		}
 		w.Write([]byte("\ndetail page"))
 	})
+	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+
+		case http.MethodPost:
+
+			var err *int
+			// simulation if the server has run correctly (password is true)
+			err = nil
+
+			if err != nil {
+				message := Message{"Kesalahan server"}
+
+				w.WriteHeader(http.StatusBadGateway)
+				w.Header().Set("Content-Type", "application/json")
+
+				json.NewEncoder(w).Encode(message)
+			}
+
+			// credentials simulation
+			accountValidations := true
+
+			if accountValidations == true {
+				message := Message{"Login berhasil"}
+
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusOK)
+
+				json.NewEncoder(w).Encode(message)
+			} else {
+				message := Message{"Username/Password salah"}
+
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusOK)
+
+				json.NewEncoder(w).Encode(message)
+			}
+		default:
+			w.WriteHeader(http.StatusBadRequest)
+			message := Message{"status tidak diizinkan"}
+
+			json.NewEncoder(w).Encode(message)
+		}
+	})
 
 	fmt.Println("server berhasil dijalankan pada localhost:8080")
 	http.ListenAndServe(":8080", nil)
