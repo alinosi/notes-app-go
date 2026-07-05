ab3520a81dc5118af9b2a2d319f18ef636b20f35
alinosi <mthoriqulfadli@gmail.com>
2026-07-05 15:32:32 +0700

    Refactor main.go and note.go for improved structure and consistency in code formatting

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -2,19 +2,31 @@ package main
 
 import (
 	"backend/internal/config"
+	"backend/internal/handler"
+	"backend/internal/repository"
+	"backend/internal/service"
 	"log"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	log.Println("Starting App-Notes Backend Server...")
+	log.Println("Starting backend server....")
 
-	// 1. Initialize Database Connection
 	db := config.ConnectDB()
-
-	// defer ensures the database connection is closed gracefully when the app stops
 	defer db.Close()
 
-	log.Println("Server is ready to accept requests!")
+	// repo
+	noteRepo := repository.NewNoteRepository(db)
+	// service
+	noteService := service.NewNoteService(noteRepo)
+	// handler
+	noteHandler := handler.NewNoteHandler(noteService)
 
-	// (Nanti kode untuk menyalakan Fiber / Routing HTTP akan kita taruh di bawah sini)
+	app := fiber.New()
+
+	app.Post("/Notes", noteHandler.CreateNote)
+
+	log.Println("Server is ready to accept request on http://localhost:3000")
+	log.Fatal(app.Listen(":3000"))
 }
--- backend/internal/model/note.go
+++ backend/internal/model/note.go
@@ -4,10 +4,10 @@ import "time"
 
 type Note struct {
 	ID        string     `json:"id"`
-	UserID    string     `json:"user_id"`
-	Title     string     `json:"title"`
-	Content   string     `json:"content"`
-	CreatedAt time.Time  `json:"created_at"`
-	UpdatedAt time.Time  `json:"updated_at"`
-	DeletedAt *time.Time `json:"deleted_at,omitempty"` // allowed NULL
+	UserID    string     `json:"user_id" db:"user_id"`
+	Title     string     `json:"title" db:"title"`
+	Content   string     `json:"content" db:"content"`
+	CreatedAt time.Time  `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at" db:"updated_at"`
+	DeletedAt *time.Time `json:"deleted_at,omitempty" db:"deleted_at"` // allowed NULL
 }
