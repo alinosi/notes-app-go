c680bf8911b420e41143726bd4bf60c8597c5794
alinosi <mthoriqulfadli@gmail.com>
2026-07-05 11:49:05 +0700

    Implement user creation logic in UserService; add email uniqueness check and fix password handling

--- backend/internal/repository/user_repository.go
+++ backend/internal/repository/user_repository.go
@@ -9,7 +9,9 @@ import (
 type UserRepository interface {
 	CreateUser(user *model.User) error
 	UpdateUser(user *model.User) error
-	ReadUser(user *model.User) error
+	GetUserByEmail(email string) (bool, error)
+	// ReadUser(user *model.User) error
+	// ReadUsers(user *model.User) error
 }
 
 type userRepositoryImpl struct {
@@ -22,6 +24,50 @@ func NewUserRepository(db *sqlx.DB) UserRepository {
 	}
 }
 
-func (r *userRepositoryImpl) CreateUser(user *model.User) error { return nil }
+func (r *userRepositoryImpl) CreateUser(user *model.User) error {
+	query := `
+		INSERT INTO user (email, username, passowrd)
+		VALUES (:email, :username, :password)
+		RETURNING id, created_at, updated_at
+	`
+
+	rows, err := r.db.NamedQuery(query, user)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	if rows.Next() {
+		err = rows.StructScan(user)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 func (r *userRepositoryImpl) UpdateUser(user *model.User) error { return nil }
-func (r *userRepositoryImpl) ReadUser(user *model.User) error   { return nil }
+func (r *userRepositoryImpl) GetUserByEmail(email string) (bool, error) {
+	query := `SELECT id FROM user WHERE email = $1`
+
+	var id string
+
+	err := r.db.Get(&id, query, email)
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
+func (r *userRepositoryImpl) ReadUsers(user *model.User) (bool, error) {
+	query := `SELECT FROM user WHERE email = :email`
+
+	err := r.db.Get(&user, query, user.Email)
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
--- backend/internal/service/user_service.go
+++ backend/internal/service/user_service.go
@@ -1,11 +1,48 @@
 package service
 
 import (
+	"backend/internal/model"
 	"backend/internal/repository"
+	"errors"
+	"unicode/utf8"
 )
 
-type UserService interface{}
+type UserService interface {
+	CreateUser(email, username, password string) (*model.User, error)
+	// CreateUser(email, username, password string) (*model.User, error)
+	// CreateUser(email, username, password string) (*model.User, error)
+}
 
 type userServiceImpl struct {
 	repo repository.UserRepository
+}
+
+func (s *userServiceImpl) CreateUser(email, username, passowrd string) (*model.User, error) {
+	if utf8.RuneCountInString(passowrd) == 0 {
+		return nil, errors.New("passowrd cannot be empty")
+	}
+
+	user := &model.User{
+		Username: username,
+		Email:    email,
+		Passowrd: passowrd,
+	}
+
+	if mark, _ := s.repo.GetUserByEmail(user.Email); mark == true {
+		return nil, errors.New("Email already used")
+	}
+
+	err := s.repo.CreateUser(user)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func NewUserService(repo repository.UserRepository) UserService {
+	return &userServiceImpl{
+		repo: repo,
+	}
 }
